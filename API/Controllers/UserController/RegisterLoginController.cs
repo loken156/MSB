6ba3bca486f3c06609837b92e2cc37914bc73125
Robert Svart <robnoir@Roberts-MacBook-Air.local>
2024-04-03 08:24:37 +0200

    from mac to pc

--- API/Controllers/UserController/RegisterLoginController.cs
+++ API/Controllers/UserController/RegisterLoginController.cs
@@ -18,14 +18,16 @@ namespace API.Controllers.UserController
         private readonly IUserRepository _userRepository;
         private readonly UserValidations _userValidations;
         private readonly LogInValidations _logInValidations;
+        private readonly ILogger<RegisterLoginController> _logging;
 
-        public RegisterLoginController(IMediator mediator, IConfiguration configuration, IUserRepository userRepository, UserValidations validations, LogInValidations logInValidations)
+        public RegisterLoginController(IMediator mediator, IConfiguration configuration, IUserRepository userRepository, UserValidations validations, LogInValidations logInValidations, ILogger<RegisterLoginController> logger)
         {
             _configuration = configuration;
             _mediator = mediator;
             _userRepository = userRepository;
             _userValidations = validations;
             _logInValidations = logInValidations;
+            _logging = logger;
         }
         //------------------------------------------------------------------------------------
 
@@ -47,7 +49,8 @@ namespace API.Controllers.UserController
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                 _logging.LogError(ex, "Error occurred during registration.");
+                return StatusCode(500, new { Message = "An error occurred while processing your request." });
             }
         }
 
@@ -81,15 +84,18 @@ namespace API.Controllers.UserController
             }
             catch (KeyNotFoundException)
             {
+                _logging.LogWarning("Attempted login for non-existing user."); // Log with context, avoiding sensitive data
                 return NotFound(new { Message = "User not found" });
             }
             catch (UnauthorizedAccessException)
             {
+                _logging.LogWarning("Unauthorized login attempt."); // Log without exposing sensitive details
                 return Unauthorized(new { Message = "Invalid Email or Password" });
             }
             catch (Exception ex)
             {
-                return BadRequest(new { Message = ex.Message });
+                _logging.LogError(ex, "Error occurred during login."); // Log error securely
+                return BadRequest(new { Message = "An error occurred while processing your request." });
             }
         }
     }
