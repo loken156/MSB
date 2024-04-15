1a2573b36f851f219b674ed044af7c7aa245479f
TomasLehtela <friscobrodet@gmail.com>
2024-04-15 12:16:31 +0200

    Added tests Add/Delete/Update for DriverCommandHandlers

---
+++ Tests/Application/Driver/CommandHandlers/AddDriverCommandHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.Commands.Driver.AddDriver;
+using Domain.Models.Driver;
+using Infrastructure.Repositories.DriverRepo;
+using Moq;
+
+namespace Tests.Application.Driver.CommandHandlers
+{
+    public class AddDriverCommandHandlerTests
+    {
+        [Fact]
+        public void Handle_GivenValidCommand_CallsAddDriverOnRepository()
+        {
+            // Arrange
+            var mockDriverRepository = new Mock<IDriverRepository>();
+            var handler = new AddDriverCommandHandler(mockDriverRepository.Object);
+            var command = new AddDriverCommand { DriverId = Guid.NewGuid() };
+
+            // Act
+            handler.Handle(command);
+
+            // Assert
+            mockDriverRepository.Verify(repo => repo.AddDriver(It.IsAny<DriverModel>()), Times.Once);
+        }
+
+        [Fact]
+        public void Handle_GivenValidCommand_PassesCorrectDriverIdToRepository()
+        {
+            // Arrange
+            var mockDriverRepository = new Mock<IDriverRepository>();
+            var handler = new AddDriverCommandHandler(mockDriverRepository.Object);
+            var command = new AddDriverCommand { DriverId = Guid.NewGuid() };
+
+            // Act
+            handler.Handle(command);
+
+            // Assert
+            mockDriverRepository.Verify(repo => repo.AddDriver(It.Is<DriverModel>(d => d.Id == command.DriverId.ToString())), Times.Once);
+        }
+
+        [Fact]
+        public void Handle_GivenInvalidCommand_ThrowsException()
+        {
+            // Arrange
+            var mockDriverRepository = new Mock<IDriverRepository>();
+            mockDriverRepository.Setup(repo => repo.AddDriver(It.IsAny<DriverModel>())).Throws<Exception>();
+            var handler = new AddDriverCommandHandler(mockDriverRepository.Object);
+            var command = new AddDriverCommand { DriverId = Guid.NewGuid() };
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => handler.Handle(command));
+        }
+    }
+}
---
+++ Tests/Application/Driver/CommandHandlers/DeleteDriverCommandHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.Commands.Driver.DeleteDriver;
+using Infrastructure.Repositories.DriverRepo;
+using Moq;
+
+namespace Tests.Application.Driver.CommandHandlers
+{
+    public class DeleteDriverCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_GivenValidCommand_CallsDeleteDriverOnRepository()
+        {
+            // Arrange
+            var mockDriverRepository = new Mock<IDriverRepository>();
+            var handler = new DeleteDriverCommandHandler(mockDriverRepository.Object);
+            var command = new DeleteDriverCommand { DriverId = Guid.NewGuid() };
+
+            // Act
+            await handler.Handle(command);
+
+            // Assert
+            mockDriverRepository.Verify(repo => repo.DeleteDriver(command.DriverId), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidCommand_PassesCorrectDriverIdToRepository()
+        {
+            // Arrange
+            var mockDriverRepository = new Mock<IDriverRepository>();
+            var handler = new DeleteDriverCommandHandler(mockDriverRepository.Object);
+            var command = new DeleteDriverCommand { DriverId = Guid.NewGuid() };
+
+            // Act
+            await handler.Handle(command);
+
+            // Assert
+            mockDriverRepository.Verify(repo => repo.DeleteDriver(command.DriverId), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_GivenInvalidCommand_ThrowsException()
+        {
+            // Arrange
+            var mockDriverRepository = new Mock<IDriverRepository>();
+            mockDriverRepository.Setup(repo => repo.DeleteDriver(It.IsAny<Guid>())).Throws<Exception>();
+            var handler = new DeleteDriverCommandHandler(mockDriverRepository.Object);
+            var command = new DeleteDriverCommand { DriverId = Guid.NewGuid() };
+
+            // Act & Assert
+            await Assert.ThrowsAsync<Exception>(() => handler.Handle(command));
+        }
+    }
+}
+
