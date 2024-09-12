a7beae9a5b7973dcb2173e0ce093c847445e5054
ArsentyStreltsov <arsentystreltsov@gmail.com>
2024-09-12 08:27:12 +0200

    add login loguot page and made it work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Удаляем токен из localStorage
+    navigate('/login'); // Перенаправляем на страницу логина
+  };
+
+  // Проверка роли пользователя
+  const isAdmin = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decoded = jwtDecode(token); // Декодируем токен
+      return decoded.role && decoded.role.includes('Admin'); // Проверяем наличие роли Admin
+    }
+    return false;
+  };
+
   return (
     <nav>
       <ul>
@@ -8,9 +26,20 @@ const Navbar = () => {
         <li><Link to="/prices">Prices</Link></li>
         <li><Link to="/services">Our Services</Link></li>
         <li><Link to="/faq">FAQ</Link></li>
+        {isAdmin() && <li><Link to="/admin">Admin</Link></li>} {/* Ссылка видна только админам */}
+        <li>
+          {localStorage.getItem('token') ? (
+            <>
+              <Link to="/my-account">My Account</Link>
+              <button onClick={handleLogout} style={{ marginLeft: '10px' }}>Logout</button>
+            </>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
---
+++ client/src/pages/MyAccountPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/MyAccountPage.jsx
+
+function MyAccountPage() {
+  return (
+    <div className="accountContainer">
+      <h1 className="heading">My Account</h1>
+      <p>Welcome to your account dashboard!</p>
+    </div>
+  );
+}
+
+export default MyAccountPage;
